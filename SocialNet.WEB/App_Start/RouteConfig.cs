ec6b2a0c96ced9450405aa72a4f9d87beb078b50
Andrey Gaidzel <andrey.gaidzel@gmail.com>
2018-05-08 11:55:50 +0300

    Change project settings

--- SocialNet.WEB/App_Start/RouteConfig.cs
+++ SocialNet.WEB/App_Start/RouteConfig.cs
@@ -11,13 +11,17 @@ namespace SocialNet.WEB
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.MapRoute("Index", "", new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+            
+            routes.MapRoute("", "{*catchall}", new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
